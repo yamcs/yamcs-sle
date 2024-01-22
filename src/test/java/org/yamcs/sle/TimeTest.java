a1f0e39c9d2bdb5812c0a0b31428a3a05b7ca6e6
Nicolae Mihalache <xpromache@gmail.com>
2024-01-22 13:09:10 +0100

    added extra test

--- src/test/java/org/yamcs/sle/TimeTest.java
+++ src/test/java/org/yamcs/sle/TimeTest.java
@@ -27,4 +27,17 @@ public class TimeTest {
 
         assertEquals(inst1, inst2);
     }
+
+    @Test
+    public void test2() {
+
+        Instant inst1 = TimeEncoding.parseHres("2021-01-01T00:01:02.123456891");
+        Timestamp ts = TimeEncoding.toProtobufTimestamp(inst1);
+
+        CcsdsTime ccsdsTime = CcsdsTime.fromUnix(ts.getSeconds(), ts.getNanos());
+        Instant inst2 = AbstractTmSleLink.toInstant(ccsdsTime);
+
+        assertEquals(inst1, inst2);
+
+    }
 }
